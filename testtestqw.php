8b339e9f01dbdc94a3e6681ee778d1a6c95da7e0
subhashish-s <subhashish.shrivas@gmail.com>
2016-11-15 16:16:39 +0530

    Add files via upload

---
+++ testtestqw.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+
+class Car {
+	function Car() {
+		$this->model = "BMW";
+	}
+}
+
+$Toyota = new Car();
+
+echo $Toyota -> model;
+
+echo("<br/>");
+echo "str1" ." ". 14777 ;
+echo strlen("hello world"."     "."khatal");
+
+echo strpos("Hello world!", "world");
+?> 
+
+
+?>
+
+
+
+
+
+</body>
+</html>
+
+</body>
+</html>
